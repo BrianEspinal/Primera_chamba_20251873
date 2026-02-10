5cc9c37b3c00a0b5b397399f6e8f0a7756eebdc2
BrianEspinal <20251873@itla.edu.do>
2026-02-10 06:19:56 -0400

    Aqui utilizamos Do While otra vez
    en esta ocasion le di muchas vueltas pues la logica estaba ahi pero estaba en un pequeno cuello de botella al declarar la variable dentro de la estructura de control, y al convertirlo por el int.parse s

---
+++ learning_program_1/Par_Impar/par_impar.cs
@@ -0,0 +1,19 @@
+﻿//Brian Espinal Polanco ---> 20251873 --->10/02/2026
+//===============================================//
+
+int numero = 0; // declaramos nuestra variable numero fuera del while para evitar posibles bucles
+do // aqui comienza nuestro buclq
+{
+    Console.Write("Ingresa un número: "); //establcemos nuestro 1er estatement
+    numero = int.Parse(Console.ReadLine()); //capturamos el valor de la variable numero
+
+    if (numero % 2 == 0) //número módulo 2 es igual a 0 es par
+    {
+        Console.WriteLine("El número es par."); 
+    }
+    else
+    {
+        Console.WriteLine("El número es impar."); // de lo contrario es impar
+    }
+     
+} while (numero % 2 != 0); //mientras el resto sea diferente de 0 el bucle continua 
